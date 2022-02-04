d4fb012fcdcb38d06266e5c71d5c894fdff4ee0e
MaksBorys <maksymilianboryslawski@gmail.com>
2022-02-04 19:09:06 +0100

    ty

--- app.py
+++ app.py
@@ -13,41 +13,40 @@ print(" 1. Server\n 2. Client\n 3. Exit\n")
 user_choice = input("Select mode: ")
 
 
-def server(port):
+def server(ip,port):
 
     app = Flask(__name__)
 
-    print('\nRunning on http://127.0.0.1:%s/ (Press CTRL+C to quit)' % str(sys.argv[1]))
+    print('\nRunning on http://%s:%s/ (Press CTRL+C to quit)' % (ip,port))
 
     logging.getLogger("werkzeug").disabled = True
     os.environ["WERKZEUG_RUN_MAIN"] = "true"
-
     @app.route('/', methods=['GET', 'POST'])
     def chat():
         request_data = request.json
         print(request_data)
         return request_data
     
-    app.run(debug=True, port=port, use_reloader=False)
+    app.run(debug=True, port=port, use_reloader=False, host=ip)
     print('\nGoodbye!')
     
 
-def client(port):
+def client(ip,port):
         
         username = input("Enter username:")
         print('=====[TO EXIT CTRL+C]=====')
         while True:
             data = input('Message: \n')
-            requests.post(url='http://localhost:%s' % (port), json='[%s] - %s' % (username,data))
+            requests.post(url='http://%s:%s' % (ip,port), json='[%s] - %s' % (username,data))
         
 
 
 if user_choice == '1':
-    server(sys.argv[1])
+    server(sys.argv[1],sys.argv[2])
     
 if user_choice == '2':
     try:
-        client(sys.argv[1])
+        client(sys.argv[1], sys.argv[2])
     except requests.exceptions.ConnectionError:
         print('Server is not available!')
     except KeyboardInterrupt:
