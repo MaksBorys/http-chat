7506d9ef5c90ce230342a126f14222d039d9dbca
MaksBorys <maksymilianboryslawski@gmail.com>
2022-02-08 13:24:27 +0100

    Bugs correction

--- app.py
+++ app.py
@@ -6,49 +6,41 @@ import sys
 import os
 
 
-h = Figlet(font="slant")
-print(h.renderText("HTTP CHAT"))
-print(" 1. Server\n 2. Client\n 3. Exit\n")
 
-user_choice = input("Select mode: ")
+def hello():
+    h = Figlet(font="slant")
+    print(h.renderText("HTTP CHAT"))
 
 
-def server(ip,port):
-
+if len(sys.argv) == 2:
     app = Flask(__name__)
 
-    print('\nRunning on http://%s:%s/ (Press CTRL+C to quit)' % (ip,port))
+    hello()
+    print('Listening on port %s (Press CTRL+C to quit)' % str(sys.argv[1]))
 
     logging.getLogger("werkzeug").disabled = True
     os.environ["WERKZEUG_RUN_MAIN"] = "true"
+
     @app.route('/', methods=['GET', 'POST'])
     def chat():
-        request_data = request.json
-        print(request_data)
+        if request.method == 'POST':
+            request_data = request.get_json(force=True)
+            print(request_data)
         return request_data
-    
-    app.run(debug=True, port=port, use_reloader=False, host=ip)
+    app.run(port=sys.argv[1], host='0.0.0.0',use_reloader=False)
     print('\nGoodbye!')
     
-
-def client(ip,port):
-        
+elif len(sys.argv) == 3:
+    try:
+        hello()
         username = input("Enter username:")
         print('=====[TO EXIT CTRL+C]=====')
         while True:
             data = input('Message: \n')
-            requests.post(url='http://%s:%s' % (ip,port), json='[%s] - %s' % (username,data))
-        
-
-
-if user_choice == '1':
-    server(sys.argv[1],sys.argv[2])
-    
-if user_choice == '2':
-    try:
-        client(sys.argv[1], sys.argv[2])
-    except requests.exceptions.ConnectionError:
+            r = requests.post(url='http://%s:%s/' % (sys.argv[1],sys.argv[2]), json='[%s] - %s' % (username,data))
+    except requests.exceptions.ConnectionError as e:
         print('Server is not available!')
+        print(e)
     except KeyboardInterrupt:
         print('\nGoodbye!')
         sys.exit()
@@ -56,6 +48,10 @@ if user_choice == '2':
         print('We have error!')
         print('Details: %s' % (e))
 
-if user_choice == '3':
-    sys.exit()
+
+
+
+
+
+
 
